991830c6ef4066912b34d3c7e0664e397979caf6
otsukakenta <k.o.k.o@i.softbank.jp>
2020-11-30 20:29:38 +0900

    react_環境構築

---
+++ app/presentationals/List/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class List extends React.Component {
+    render () {
+        return (
+            <>
+                <p>Hello React!</p>
+                <p>testtest</p>
+                <p>testtest</p>
+                <p>testtest</p>
+            </>
+        );
+    }
+}
+
+export default List;
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = {
+    mode: 'development',
+    target: 'node',
+    entry: {
+        'main': path.resolve(__dirname, 'app/index.js'),
+    },
+    output: {
+        filename: '[name].[hash].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /(\.js|\.jsx)$/,
+                use: [
+                    {
+                        // Babel を利用する
+                        loader: 'babel-loader',
+                        // Babel のオプションを指定する
+                        options: {
+                            presets: [
+                                // プリセットを指定することで、ES2020 を ES5 に変換
+                                '@babel/preset-env',
+                                // React の JSX を解釈
+                                '@babel/react'
+                            ]
+                        }
+                    }
+                ],
+                exclude: /node_modules/
+            },
+            // {
+            //     test: /\.(scss|css)$/,
+            //     use: [
+            //         MiniCssExtractPlugin.loader,
+            //         // 'style-loader',
+            //         'css-loader?modules'
+            //         // {
+            //         //     loader: 'sass-loader',
+            //         //     options: {
+            //         //         sourceMap: (IS_PROD) ? false : true
+            //         //     }
+            //         // },
+            //         // {
+            //         //     loader: 'sass-resources-loader',
+            //         //     options: {
+            //         //         resources: [
+            //         //             path.resolve(__dirname, 'src/styleConfig.scss')
+            //         //         ]
+            //         //     }
+            //         // }
+            //     ]
+            // },
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    // node: {
+    //     fs: 'empty',
+    // },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.resolve(__dirname, 'assets/index.html'),
+        }),
+        // new MiniCssExtractPlugin({
+        //     filename: 'dist/style.css'
+        // })
+    ],
+};
+
+module.exports = config;
